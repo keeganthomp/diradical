746ef80838918f30747a13c5e2fe9fef813bc7ce
Keegan Thompson <whereiskeegan@gmail.com>
2022-12-04 19:43:53 -0800

    last cleanup for now...

--- hooks/useS3.ts
+++ hooks/useS3.ts
@@ -9,43 +9,37 @@ export type FolderOptions = 'singles' | 'album'
 
 export default function useS3() {
   const uploadToS3 = async (
-    files: any,
+    file: any,
     title: string,
     walletAddress: string,
     parentFolder: FolderOptions = 'singles',
-  ): Promise<string | string[]> => {
-    const filesToUpload = Array.isArray(files) ? files : [files]
-    const results: any[] = []
+  ): Promise<string> => {
     try {
-      for await (const fileUpload of filesToUpload) {
-        const fileExtension = fileUpload.name.split('.').pop()
-        const fileNameWithoutExt = fileUpload.name.replace(/\.[^/.]+$/, '')
-        // replace spaces with underscores
-        const fmtFileName = fileNameWithoutExt.replace(/ /g, '_')
-        // i.e. me.jpeg becomes me_123e4567-e89b-12d3-a456-426655440000.jpeg
-        const fileNameWithUid = `${fmtFileName}_${uuidv4()}.${fileExtension}`
-        const titleWithUuid = `${title}_${uuidv4()}`
-        const folder = `${parentFolder}/${titleWithUuid}`
-        const { data } = await axios(
-          BASE_PRESIGNED_API_URL +
-            '?' +
-            `fileName=${fileNameWithUid}` +
-            `&folder=${folder}` +
-            `&wallet=${walletAddress}`,
-        )
-        const { url: presignedUrl } = data
-        await axios.put(presignedUrl, fileUpload)
-        const key = S3.createKey({
-          fileName: fileNameWithUid,
-          folder,
-          walletAddress,
-        })
-        results.push(`${BASE_S3_URL}/${key}`)
-      }
-      return results.length === 1 ? results[0] : results
+      const fileExtension = file.name.split('.').pop()
+      const fileNameWithoutExt = file.name.replace(/\.[^/.]+$/, '')
+      // replace spaces with underscores
+      const fmtFileName = fileNameWithoutExt.replace(/ /g, '_')
+      // i.e. me.jpeg becomes me_123e4567-e89b-12d3-a456-426655440000.jpeg
+      const fileNameWithUid = `${fmtFileName}_${uuidv4()}.${fileExtension}`
+      const titleWithUuid = `${title}_${uuidv4()}`
+      const folder = `${parentFolder}/${titleWithUuid}`
+      const { data } = await axios(
+        BASE_PRESIGNED_API_URL +
+          '?' +
+          `fileName=${fileNameWithUid}` +
+          `&folder=${folder}` +
+          `&wallet=${walletAddress}`,
+      )
+      const { url: presignedUrl } = data
+      await axios.put(presignedUrl, file)
+      const key = S3.createKey({
+        fileName: fileNameWithUid,
+        folder,
+        walletAddress,
+      })
+      return `${BASE_S3_URL}/${key}`
     } catch {
       throw new Error('Unable to get presigned URL')
-    } finally {
     }
   }
 
